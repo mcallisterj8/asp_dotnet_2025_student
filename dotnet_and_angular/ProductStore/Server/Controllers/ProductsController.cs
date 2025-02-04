1a6de2543adaf6df4286288a616a38957fc509af
Jacob McAllister <mcallisterj@westerntc.edu>
2025-02-04 10:52:51 -0600

    Seed data.
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- dotnet_and_angular/ProductStore/Server/Controllers/ProductsController.cs
+++ dotnet_and_angular/ProductStore/Server/Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Models;
 
 namespace Server.Controllers
 {
@@ -8,11 +11,17 @@ namespace Server.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context) {
+            _context = context;
+        }
+
         //localhost:5000/api/products/
         [HttpGet("")]
-        public async Task<ActionResult> GetAllProducts(){
+        public async Task<ActionResult> GetAllProducts() {
 
-            return Ok(new {message = "Response from GetAllProducts!"});
+            return Ok(await _context.Products.ToListAsync());
         }
 
     }
