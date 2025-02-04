1d55670c2459beee3407299ef0874c7a638e2ccf
Jacob McAllister <mcallisterj@westerntc.edu>
2025-02-04 10:24:29 -0600

    In class update.
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- dotnet_and_angular/ProductStore/Client/src/app/components/products/products.component.ts
+++ dotnet_and_angular/ProductStore/Client/src/app/components/products/products.component.ts
@@ -11,9 +11,11 @@ import { CommonModule } from '@angular/common';
 export class ProductsComponent implements OnInit {
   private _productService = inject(ProductService);
 
-  ngOnInit(): void {
-    console.log("HELLO FROM COMP");
+  // By virtue of having this & how the getAllProducts() is implemented,
+  // we will have access to all products once our http call successfully returns.
+  public productList$ = this._productService.productList$;
 
+  ngOnInit(): void {    
     this._productService.getAllProducts().subscribe((res) => {
       console.log('res:', res);
     });
--- dotnet_and_angular/ProductStore/Client/src/app/services/product.service.ts
+++ dotnet_and_angular/ProductStore/Client/src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,20 @@ import { Observable } from 'rxjs';
 export class ProductService {
   private _http = inject(HttpClient);
 
+  private _productListSubject: BehaviorSubject<Product[]> =
+    new BehaviorSubject<Product[]>([] as Product[]);
+
+  public productList$: Observable<Product[]> = 
+    this._productListSubject.asObservable();
+
   constructor() { }
 
-  public getAllProducts(): Observable<any> {
-    return this._http.get<any>(`/api/products`);
+  public getAllProducts(): Observable<Product[]> {
+    return this._http.get<Product[]>(`/api/products`).pipe(
+      tap((productList) => {
+        // Place product list inside of BehaviorSubject
+        this._productListSubject.next(productList);
+      })
+    );
   }
 }
