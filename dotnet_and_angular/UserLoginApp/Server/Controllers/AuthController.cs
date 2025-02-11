e17d1033b6fcfb396c75355b8e20e1f66c45658a
Jacob McAllister <mcallisterj@westerntc.edu>
2025-02-11 11:20:31 -0600

    End of class
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- dotnet_and_angular/UserLoginApp/Server/Controllers/AuthController.cs
+++ dotnet_and_angular/UserLoginApp/Server/Controllers/AuthController.cs
@@ -2,10 +2,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Server.Models;
 
 namespace Server.Controllers
 {
-    // api/auth
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
@@ -19,6 +20,59 @@ namespace Server.Controllers
         ){
             _signInManager = signInManager;
             _userManager = userManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register([FromBody] EmailLoginDetails details){
+            // Create IdentityUser instance
+            IdentityUser user = new IdentityUser { UserName = details.Email, Email = details.Email };
+
+            // Create the user
+            IdentityResult result = await _userManager
+                .CreateAsync(user, details.Password)
+                .ConfigureAwait(false);
+
+            if(!result.Succeeded) {
+                List<string> errors = result.Errors
+                    .Select(e => e.Description)
+                    .ToList();
+
+                return BadRequest(new { errors });
+            }
+            
+            return Ok(new UserDto { Id = user.Id, UserName = user.UserName });
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login([FromBody] EmailLoginDetails details) {
+            // Retrieve user
+            IdentityUser? user = await _userManager
+                .FindByEmailAsync(details.Email);
+            
+            if(null == user) {
+                return BadRequest($"No user with username {details.Email}");
+            }
+
+            // Check if the password is correct & attempt to sign in
+            var result = await _signInManager
+                .PasswordSignInAsync(details.Email, details.Password, false, false)
+                .ConfigureAwait(false);
+
+            if(!result.Succeeded) {
+                return Unauthorized();
+            }
+
+            return Ok(new UserDto { Id = user.Id, UserName = user.UserName });
+
+        }
+
+        [HttpPost("logout")]
+        public async Task<ActionResult> Logout(){
+            await _signInManager.SignOutAsync();
+            
+            return Ok(new { message = "Logged out successfully."} );
         }
 
     }
---
+++ dotnet_and_angular/UserLoginApp/Server/Models/UserDto.cs
@@ -0,0 +1,6 @@
+namespace Server.Models;
+
+public class UserDto {
+    public required string Id { get; set; }
+    public required string UserName { get; set; }
+}
