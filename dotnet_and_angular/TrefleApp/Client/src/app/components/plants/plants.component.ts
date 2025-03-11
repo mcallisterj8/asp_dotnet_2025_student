3060dc3291015c21df0b916a620a24de7d7b02c8
Jacob McAllister <datadeltaanalysis@protonmail.com>
2025-03-10 23:50:19 -0500

    Update
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

--- dotnet_and_angular/TrefleApp/Client/src/app/components/plants/plants.component.ts
+++ dotnet_and_angular/TrefleApp/Client/src/app/components/plants/plants.component.ts
@@ -3,11 +3,12 @@ import { TrefleService } from '../../services/trefle.service';
 import { Observable } from 'rxjs';
 import { Plant } from '../../models/plant';
 import { CommonModule } from '@angular/common';
+import { PlantCardComponent } from '../plant-card/plant-card.component';
 
 @Component({
   selector: 'app-plants',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlantCardComponent],
   templateUrl: './plants.component.html',
   styleUrl: './plants.component.css',
 })
--- dotnet_and_angular/TrefleApp/Client/src/app/components/search-bar/search-bar.component.ts
+++ dotnet_and_angular/TrefleApp/Client/src/app/components/search-bar/search-bar.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
-export class SearchBarComponent {
-
-}
+export class SearchBarComponent {}
